b120b3a34449861ecbb8840ce4e45598f9a1f171
Zeferu2 <zeferu19@gmail.com>
2024-04-25 12:55:02 -0500

    file added

---
+++ dayOfTheweek.js
@@ -0,0 +1,32 @@
+let dayOfWeek = 1;
+let nameofDay;
+
+switch (dayOfWeek) {
+    case 1:
+        nameofDay = 'Sunday';
+        break;
+    case 2:
+        nameofDay = 'Monday';
+        break;
+    case 3:
+        nameofDay = 'Tuesday';
+        break;
+    case 4:
+        nameofDay = 'Wednesday';
+        break;
+    case 5:
+        nameofDay = 'Thursday';
+        break;
+    case 6:
+        nameofDay = 'Friday';
+        break;
+    case 7:
+        nameofDay = 'Saturday';
+        break;
+    default:
+        nameofDay = 'Invalid Day';
+}
+
+console.log(
+    `Day of the weak: ${nameofDay}
+    In put day of the weak: ${dayOfWeek}`);
