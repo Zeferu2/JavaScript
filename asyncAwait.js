e235b4bd664df8b48eb5f345274ce3a1f3e060b4
Zeferu2 <zeferu19@gmail.com>
2024-05-18 09:54:03 -0500

    modified-asyncAwait.js

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,31 +1,11 @@
-const students = [
-    {
-        name: "Zeferu",
-        age: 20,
-        Education: "Bachelor"
-    },
-    {
-        name: "Hagos",
-        age: 30,
-        Education: "Bachelor"
-    },
-    {
-        name: "Abrham",
-        age: 40,
-        Education: "Bachelor"
-    },
-
-];
-
-function studentsData() {
-    return students;
-}
-async function fetchStudents() {
+// what to do? fetching data from an api
+// how to do it? using fetch api.
+async function fetchProducts() {
 const response= await fetch( "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
 return response.json(); 
 console.log(data);
 }
-const result = fetchStudents();
+const result = fetchProducts();
 result.then((data) => {
 
 console.log(data);})
